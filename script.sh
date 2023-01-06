211c16096e390c3e5e0ad7686b8e09c5a31900b6
x0n4d0 <romagnolimauricio@gmail.com>
2023-01-06 01:36:13 -0300

    chore: add initial files

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# ======================================================
+# Move .cfg file to local CSGO files
+# ======================================================
+chmod +x script.sh
+
+cp *.cfg /home/$USER/.steam/debian-installation/steamapps/common/Counter-Strike\ Global\ Offensive/csgo/cfg
