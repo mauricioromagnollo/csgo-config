f647a944569ae92703f9d7ef333f887dbbe2e077
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-03-11 18:12:17 -0300

    test: add vitest structure to tests and ci step (#6)
    Close #6

---
+++ test/unit/sample.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Unit Test Sample', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true)
+  })
+
+  it('false to be false', () => {
+    expect(false).toBe(false)
+  })
+})
